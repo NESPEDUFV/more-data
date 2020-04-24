a52ee9c9639a857ba4eabe670cac43a420545be8
Germano Barcelos <gegenbarcelos@gmail.com>
2020-04-23 23:05:51 -0300

    bug: sphinx build

--- docs/source/api_connector.rst
+++ docs/source/api_connector.rst
@@ -2,12 +2,12 @@
 
 
 API Connector
-=======================
+=============
 Connector that will enrich data based on API connector. This package uses requests package. 
 
 
 API Connector
------------------------
+-------------
 
 .. py:module:: enrichment.enricher.api_connector.api_connector
 
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Germano Barcelos e Leonardo Alves'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinxcontrib.napoleon', 'sphinx_rtd_theme', 'sphinx.ext.autodoc', 'sphinx.ext.coverage']
+extensions = ['sphinx.ext.napoleon', 'sphinx_rtd_theme', 'sphinx.ext.autodoc', 'sphinx.ext.coverage']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -59,4 +59,4 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
