458a1583ad6251196a7d5fd3b9fc02ec70708ae3
Germano Barcelos <gegenbarcelos@gmail.com>
2020-04-25 00:02:20 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from setuptools import setup, find_packages
 
 install_requires = [
-    "requests"
-    "h3"
-    "pandas"
-    "numpy"
-    "elasticsearch"
+    "requests",
+    "h3",
+    "pandas",
+    "numpy",
+    "elasticsearch",
 ]
 
 docs_requires = [
-    "sphinx"
-    "sphinx-rtd-theme"
-    "jinja2"
-    "sphinxcontrib-napoleon"
+    "sphinx",
+    "sphinx-rtd-theme",
+    "jinja2",
+    "sphinxcontrib-napoleon",
 ]
 
 setup(
-    name = "enrichment"
-    version = '0.1'
-    packages = ['enrichment']
-    python_requires=">=3.6"
-    install_requires = install_requires
+    name = "enrichment",
+    version = '0.1',
+    packages = ['enrichment'],
+    python_requires=">=3.6",
+    install_requires = install_requires,
     extras_require={
         "develop": install_require + docs_require,
         "docs": docs_require,
