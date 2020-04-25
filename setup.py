56e933ebb3b03822741083948fe393bfd9d648a2
Germano Barcelos <gegenbarcelos@gmail.com>
2020-04-25 14:21:01 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ docs_requires = [
 setup(
     name = "enrichment",
     version = '0.1',
-    packages = ['enrichment'],
+    packages = find_packages(where="."),
     python_requires=">=3.6",
     install_requires = install_requires,
     extras_require={
