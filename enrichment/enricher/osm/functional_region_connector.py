8547e0d938edce7a99e00c8b566e6cfcec129ace
Germano Barcelos <gegenbarcelos@gmail.com>
2020-10-09 10:28:24 -0300

    bug: fix on functional_region_connetor dict_keys empty

--- enrichment/enricher/osm/functional_region_connector.py
+++ enrichment/enricher/osm/functional_region_connector.py
@@ -35,7 +35,7 @@ class FunctionalRegionConnector(IEnricherConnector):
     key: str
     """
 
-    def __init__(self, files, key, dict_keys):
+    def __init__(self, files, key, dict_keys=[]):
         self.key = key
         self.dict_keys = dict_keys
         self.files = files
@@ -84,15 +84,15 @@ class FunctionalRegionConnector(IEnricherConnector):
         count = 0
         for d in data.parse(**kwargs):
 
-            if len(self.dict_keys) > 0:
+            if not self.dict_keys:
+                points = d  
+            else:
                 points = d[self.dict_keys[0]]
                 for k in range(1, len(self.dict_keys)):
                     try:
                         points = points[self.dict_keys[k]]
                     except KeyError as e:
                         return None
-            else:
-                points = d  
                 
             if isinstance(points, list):
                 for point in points:
