54f56eb8520497d6a7f56182b18b35f192d8e72c
Germano Barcelos <gegenbarcelos@gmail.com>
2021-01-28 22:46:53 -0300

    adding configs file

--- setup.py
+++ setup.py
@@ -1,26 +1,30 @@
 from setuptools import setup, find_packages
+import os
 
-install_requires = [
-    "requests==2.22.0",
-    "SQLAlchemy==1.3.18",
-    "PyMySQL==0.10.0",
-    "h3==3.4.3",
-    "osm2geojson==0.1.27",
-    "Rtree==0.9.7",
-    "pyproj==2.4.2.post1",
-    "geopandas==0.7.0",
-    "geopy==2.0.0",
-    "pandas==1.0.1",
-    "numpy==1.18.1",
-    "elasticsearch==7.5.1",
-    "shapely==1.7.0",
-]
-docs_requires = [
-    "sphinx==2.4.0",
-    "sphinx-rtd-theme==0.4.3",
-    "jinja2==2.11.1",
-    "sphinxcontrib-napoleon==0.7",
-]
+if os.environ.get("READTHEDOCS", False) == "True":
+    INSTALL_REQUIRES = []
+else: 
+    INSTALL_REQUIRES = [
+        "requests==2.22.0",
+        "SQLAlchemy==1.3.18",
+        "PyMySQL==0.10.0",
+        "osm2geojson==0.1.27",
+        "Rtree==0.9.7",
+        "pyproj==2.4.2.post1",
+        "geopandas==0.7.0",
+        "geopy==2.0.0",
+        "pandas==1.0.1",
+        "numpy==1.18.1",
+        "elasticsearch==7.5.1",
+        "shapely==1.7.0",
+    ]
+
+    DOCS_REQUIRES = [
+        "sphinx==2.4.0",
+        "sphinx-rtd-theme==0.4.3",
+        "jinja2==2.11.1",
+        "sphinxcontrib-napoleon==0.7",
+    ]
 
 setup(
     name = "moredata",
@@ -29,9 +33,9 @@ setup(
     version = '0.1',
     packages = find_packages(where="."),
     python_requires=">=3.6",
-    install_requires = install_requires,
+    install_requires = INSTALL_REQUIRES,
     extras_require={
-        "develop": install_requires + docs_requires,
-        "docs": docs_requires,
+        "develop": INSTALL_REQUIRES + DOCS_REQUIRES,
+        "docs": DOCS_REQUIRES,
     },
 )
