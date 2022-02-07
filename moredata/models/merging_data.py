6ae48a944d954bee3a9e7e6a630dc504e3ee431f
thomas <thomas.sonobe@gmail.com>
2022-02-07 09:34:32 -0300

    add read

--- moredata/models/merging_data.py
+++ moredata/models/merging_data.py
@@ -1,9 +1,15 @@
-from geopandas import sjoin
+import geopandas
 class MergingData:
 
-    def __init__(data_file):
-        self.data_file = data_file
+    @classmethod
+    def from_geodataframe(self,geodataframe):
+        self.data = geopandas.datasets.get_path(geodataframe)
+        return self
 
-    def spatial_join(gdf_enricher ,gdf_enrichering,how, predicate ):
-        return sjoin(gdf_enricher, gdf_enrichering,how, predicate)
+    @classmethod
+    def from_path(self, path):
+        self.data = geopandas.read_csv(path) 
+        return self
 
+    def spatial_join(self, gdf_enricher ,gdf_enrichering,how, predicate ):
+        return geopandas.sjoin(gdf_enricher, gdf_enrichering,how, predicate)
