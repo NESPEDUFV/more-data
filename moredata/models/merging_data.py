1e2b095dc334dbf9568190f696a8304b1cbd440d
thomas <thomas.sonobe@gmail.com>
2022-02-07 11:06:56 -0300

    fix: changes returns functions in mergindata module

--- moredata/models/merging_data.py
+++ moredata/models/merging_data.py
@@ -3,13 +3,15 @@ class MergingData:
 
     @classmethod
     def from_geodataframe(self,geodataframe):
-        self.data = geopandas.datasets.get_path(geodataframe)
-        return self
+        self.data = geopandas.read_file(geopandas.datasets.get_path(geodataframe))
+        return self.data
 
     @classmethod
     def from_path(self, path):
-        self.data = geopandas.read_csv(path) 
-        return self
+        self.data = geopandas.read_file(path) 
+        return self.data
 
+    @classmethod
     def spatial_join(self, gdf_enricher ,gdf_enrichering,how, predicate ):
-        return geopandas.sjoin(gdf_enricher, gdf_enrichering,how, predicate)
+        self.data = geopandas.sjoin(gdf_enricher, gdf_enrichering,how, predicate)
+        return self.data
