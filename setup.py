cf13400a5500fb6b7263ed67c70b4b9b71a985ce
Germano Barcelos <gegenbarcelos@gmail.com>
2022-08-10 19:03:31 -0300

    chore: release 0.1.5v

--- setup.py
+++ setup.py
@@ -1,24 +1,34 @@
 from setuptools import setup, find_packages
 import os
 
+# get all data dirs in the datasets module
+data_files = []
+
+for item in os.listdir("moredata/datasets"):
+    print("")
+    if not item.startswith("__"):
+        if os.path.isdir(os.path.join("moredata/datasets/", item)):
+            data_files.append(os.path.join("datasets", item, "*"))
+
 if os.environ.get("READTHEDOCS", False) == "True":
     INSTALL_REQUIRES = []
     DOCS_REQUIRES = []
-else: 
+else:
     INSTALL_REQUIRES = [
-        "requests==2.22.0",
-        "SQLAlchemy==1.4.37",
-        "PyMySQL==0.10.0",
-        "osm2geojson==0.1.27",
+        "dask-geopandas>=0.2.0",
+        "requests>=2.22.0",
+        "SQLAlchemy>=1.4.37",
+        "PyMySQL>=0.10.0",
+        "osm2geojson>=0.1.27",
         "h3==3.4.3",
-        "Rtree==0.9.7",
-        "pyproj==2.4.2.post1",
+        "Rtree>=0.9.7",
+        "pyproj>=2.4.2.post1",
         "geopandas>=0.7.0",
-        "geopy==2.0.0",
+        "geopy>=2.0.0",
         "pandas>=1.0.1",
         "numpy>=1.18.1",
-        "elasticsearch>=7.5.1",
-        "shapely==1.7.0",
+        "elasticsearch==7.17.0",
+        "shapely>=1.7.0",
     ]
 
     DOCS_REQUIRES = [
@@ -30,10 +40,11 @@ else:
 
 setup(
     name="moredata",
-    url='https://github.com/gegen07/more-data',
-    download_url='https://github.com/gegen07/more-data/archive/v0.1.2.tar.gz',
-    version='0.1.2',
+    url="https://github.com/gegen07/more-data",
+    download_url="https://github.com/gegen07/more-data/archive/v0.1.5.tar.gz",
+    version="0.1.5",
     packages=find_packages(where="."),
+    package_data={"moredata": data_files},
     python_requires=">=3.6",
     install_requires=INSTALL_REQUIRES,
     extras_require={
